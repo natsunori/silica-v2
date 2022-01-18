08f85c177046a80ea4117a68a4bcb3a33fb805ca
Tyler <tylerrp06@gmail.com>
2022-01-18 08:21:52 +0000

    Updated uwu command
    * Improved formatting
    * Removed unused variables

--- Commands/uwu.js
+++ Commands/uwu.js
@@ -1,13 +1,11 @@
 const Discord = require('discord.js');
+
 module.exports = {
 	name: 'uwu',
 	description: 'uwu',
-	execute(message, args) {
-        message.delete()
-        const embed = new Discord.MessageEmbed()
-        message.channel.send('That\'s very uwu of you')
-		message.channel.send(`https://tenor.com/view/uwu-smug-anime-stare-gif-17603924`)
-            
-
-	},
+	execute(message, _args) {
+        message.delete();
+        message.channel.send('That\'s very uwu of you');
+		message.channel.send('https://tenor.com/view/uwu-smug-anime-stare-gif-17603924');
+	}
 };
