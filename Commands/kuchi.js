fc6f9d70ce19d8c6232692486854a6030a18f24f
Tyler <tylerrp06@gmail.com>
2022-01-18 18:05:07 +0000

    Updated kuchi command
    * Improved formatting
    * Removed unnecessary code
    * Removed unused variables

--- Commands/kuchi.js
+++ Commands/kuchi.js
@@ -1,16 +1,10 @@
-const Discord = require('discord.js');
 module.exports = {
 	name: 'kuchi',
 	description: 'Bruh\'s ',
-	execute(message, args) {
-        message.delete()
-        if(message.author.id === "630481836665208835" || message.author.id === "339838960484483072"){
-        const embed = new Discord.MessageEmbed()
-		message.channel.send(`You're a beta male cuck`);
-       }
-     
-        else return  
-
-	},
+	execute(message, _args) {
+        message.delete();
+        if (message.author.id === "630481836665208835" || message.author.id === "339838960484483072") {
+			message.channel.send('You\'re a beta male cuck');
+        }
+	}
 };
-
