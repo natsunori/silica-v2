635d6d32d57f96705e2bc34e72fd68dcbd927a45
Tyler <tylerrp06@gmail.com>
2022-01-18 19:19:33 +0000

    Updated courbi command
    * Improved formatting
    * Removed unnecessary code
    * Made quotation marks consistent

--- Commands/courbi.js
+++ Commands/courbi.js
@@ -1,15 +1,13 @@
 const Discord = require('discord.js');
+
 module.exports = {
 	name: 'courbi',
 	description: 'Bruh\'s ',
-	execute(message, args) {
-        message.delete()
-        if(message.author.id === "630481836665208835" || message.author.id === "502250268558229534"){
-        const embed = new Discord.MessageEmbed()
-		message.channel.send(`you're very cute you fucking cunt, ily`);
-       }
-     
-        else return  
-
-	},
-};
+	execute(message, _args) {
+		message.delete()
+		if (message.author.id === "630481836665208835" || message.author.id === "502250268558229534") {
+			const embed = new Discord.MessageEmbed()
+			message.channel.send('you\'re very cute you fucking cunt, ily');
+		}
+	}
+}
