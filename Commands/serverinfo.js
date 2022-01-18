bc5d129a3c43456644a2f11a9866f1cad562a834
Tyler <tylerrp06@gmail.com>
2022-01-18 08:33:04 +0000

    Updated serverinfo command
    * Improved formatting
    * Updated command description
    * Made quotation marks consistent
    * Removed unnecessary "Loading data" message
    * Removed unnecessary "stopTyping" function (discord.js doesn't automatically execute the "startTyping" function)

--- Commands/serverinfo.js
+++ Commands/serverinfo.js
@@ -1,15 +1,13 @@
-const Discord = require('discord.js'), { prefix, colors } = require("../config.json");
-    module.exports = {
-        name: 'serverinfo',
-        description: 'Ping!',
-        execute(message, args){
-            message.channel.send('Loading data').then (async (msg) =>{
-                msg.delete()
-                const embed = new Discord.MessageEmbed()
+const Discord = require('discord.js'), { prefix, colors } = require('../config.json');
+
+module.exports = {
+    name: 'serverinfo',
+    description: 'View your servers information',
+    execute(message, _args) {
+        const embed = new Discord.MessageEmbed()
             .setColor('#eb34e8')
             .setTitle('Silica')
             .addField(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-                message.channel.send(embed).then(() => { message.channel.stopTyping() })
-            })
-        }
+        message.channel.send(embed);
     }
+}
