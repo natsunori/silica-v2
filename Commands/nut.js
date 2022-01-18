10e8c42e7ccbe5cc70e1ca4292b56ab3f5ea309e
Tyler <tylerrp06@gmail.com>
2022-01-18 08:40:24 +0000

    Updated nut command
    * Removed unused variables
    * Improved formatting
    * Made quotation marks consistent

--- Commands/nut.js
+++ Commands/nut.js
@@ -1,13 +1,9 @@
-const Discord = require('discord.js');
 module.exports = {
 	name: 'nut',
 	description: 'Bruh\'s ',
-	execute(message, args) {
-        message.delete()
-        const embed = new Discord.MessageEmbed()
-		message.channel.send(`Someone hit the nut button`);
-        message.channel.send("https://tenor.com/view/nut-button-anime-press-press-button-gif-17216779")
-            
-
-	},
+	execute(message, _args) {
+        message.delete();
+		message.channel.send('Someone hit the nut button');
+        message.channel.send('https://tenor.com/view/nut-button-anime-press-press-button-gif-17216779');
+	}
 };
