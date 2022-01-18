765e76824888c4de3d54260c6e797f7f56340507
Tyler <tylerrp06@gmail.com>
2022-01-18 08:46:18 +0000

    Updated mainfin command
    * Improved formatting
    * Updated command description

--- Commands/mainfin.js
+++ Commands/mainfin.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js');
+
 module.exports = {
     name: 'mainfin',
-    description: 'Ping!',
-    execute(message, args){
-        message.delete()
+    description: 'Executed when planned maintenance has been completed',
+    execute(message, _args){
+        message.delete();
         const embed = new Discord.MessageEmbed()
-        .addField("ALERT           ", `${'Silica is back up from planned maintainance thank you for your patience, contact Rhys llewellyn #3527 for any bug reports'}` , true)
-        message.channel.send(embed)
+            .addField('ALERT           ', 'Silica is back up from planned maintainance thank you for your patience, contact Rhys llewellyn #3527 for any bug reports', true);
+        message.channel.send(embed);
     }
 }
