58e0a1fa122ac1b4c3c4293fb36ba67518f82a6e
Tyler <tylerrp06@gmail.com>
2022-01-18 19:13:53 +0000

    Updated avatar command
    * Improved formatting
    * Removed unnecessary code
    * Made quotation marks consistent
    * Added comments
    * Removed callback hell (callbackhell.com)
    * Cleaned code

--- Commands/avatar.js
+++ Commands/avatar.js
@@ -1,18 +1,31 @@
 const { MessageEmbed } = require('discord.js');
+
 module.exports = {
 	name: 'avatar',
 	description: 'Get the avatar URL of the tagged user(s), or your own avatar.',
-	execute(message){
+	execute(message) {
         const client = message.client;
-        let mention = message.mentions.users.first(), user1 = message.author.id;
-        if (mention){user1 = mention.id}
-        try { user1 = client.users.cache.find(user => user.id === user1) } catch(error) { message.reply(`${user1} is not a valid userid.`).then (async (msg) =>{ setTimeout(function(){msg.delete() }, 3000) }).then(() => {message.channel.stopTyping()})}
+        let mention = message.mentions.users.first(),
+            user1 = message.author.id;
+
+        if (mention)
+            user1 = mention.id
+        try {
+            user1 = client.users.cache.find(user => user.id === user1);
+        } catch(error) {
+            // Send the user an error message
+            let msg = await message.reply(`${user1} is not a valid user ID`);
+            // Delete the message after a set amount of time
+            setTimeout(() => msg.delete(), 3000);
+        }
+
         const embed = new MessageEmbed()
-        .setColor('#eb34e8')
-        .setAuthor('Avatar')
-        .setImage(user1.displayAvatarURL({ dynamic: true }))
-        .addField("Your avatar can be found here", `[PNG](${user1.displayAvatarURL({ format: "png", size: 4096 })}) | [JPG](${user1.displayAvatarURL({ format: "jpg", size: 4096 })}) | [WEBP/GIF](${user1.displayAvatarURL({ dynamic: true, size: 4096 })})`)
-        .setFooter(`Requested by ${message.author.tag}`)
-        message.channel.send(embed).then(() => {message.channel.stopTyping()})
+            .setColor('#eb34e8')
+            .setAuthor('Avatar')
+            .setImage(user1.displayAvatarURL({ dynamic: true }))
+            .addField('Your avatar can be found here', `[PNG](${user1.displayAvatarURL({ format: 'png', size: 4096 })}) | [JPG](${user1.displayAvatarURL({ format: 'jpg', size: 4096 })}) | [WEBP/GIF](${user1.displayAvatarURL({ dynamic: true, size: 4096 })})`)
+            .setFooter(`Requested by ${message.author.tag}`);
+
+        message.channel.send(embed);
     }
-};
+}
