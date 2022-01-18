d2287563e53a02b9544a18aff426d19e2aed4f66
Tyler <tylerrp06@gmail.com>
2022-01-18 19:18:08 +0000

    Updated bruh command
    * Improved formatting
    * Removed unused variables

--- Commands/bruh.js
+++ Commands/bruh.js
@@ -1,12 +1,8 @@
-const Discord = require('discord.js');
 module.exports = {
 	name: 'bruh',
 	description: 'Bruh\'s ',
-	execute(message, args) {
-        message.delete()
-        const embed = new Discord.MessageEmbed()
-		message.channel.send(`Bruh`);
-            
-
+	execute(message, _args) {
+        message.delete();
+		message.channel.send('Bruh');
 	},
-};
+}
