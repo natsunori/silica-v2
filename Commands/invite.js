e38b7a8372499b5f0e3bd49541bc3711d39d60fd
Tyler <tylerrp06@gmail.com>
2022-01-18 18:08:47 +0000

    Updated invite command
    * Made quotation marks consistent
    * Improved formatting
    * Removed unused imports
    * Removed unnecessary "Loading data" message (all data collected is cached therefore sending a "loading data" message is unnecessary)
    * Removed unnecessary "stopTyping" function (discord.js doesn't automatically execute the "startTyping" function)

--- Commands/invite.js
+++ Commands/invite.js
@@ -1,14 +1,10 @@
-const Discord = require('discord.js'), { prefix, colors } = require("../config.json");
+const Discord = require('discord.js'), { prefix, colors } = require('../config.json');
 module.exports = {
 	name: 'invite',
-	description: 'invite',
-	execute(message, args){
-        const client = message.client
-        message.channel.send('Loading data').then (async (msg) =>{
-            msg.delete()
-            const embed = new Discord.MessageEmbed()
-            .addField("**Join the STS discord:          **", `${'https://discord.gg/cRu8VUPFdU'}` , true)
-            message.channel.send(embed).then(() => { message.channel.stopTyping() })
-        })
+	description: 'View the bots invite link',
+	execute(message, _args){
+        const embed = new Discord.MessageEmbed()
+            .addField('**Join the STS discord:          **', 'https://discord.gg/cRu8VUPFdU', true);
+        message.channel.send(embed);
     }
 }
