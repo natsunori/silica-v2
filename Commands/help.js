31b49998f716315ce9f34cdd60094dae4819f03f
Tyler <tylerrp06@gmail.com>
2022-01-18 18:13:07 +0000

    Updated help commannd
    * Removed unused variables
    * Removed unnecessary "stopTyping" function (discord.js doesn't automatically execute the "startTyping" function)
    * Removed unnecessary "Loading data" message (all data collected is cached therefore sending a "loading data" message is unnecessary)
    * Updated command description
    * Improved formatting

--- Commands/help.js
+++ Commands/help.js
@@ -1,27 +1,24 @@
-const Discord = require('discord.js'), { prefix, colors } = require("../config.json");
+const Discord = require('discord.js');
+
 module.exports = {
 	name: 'help',
-	description: 'Ping!',
-	execute(message, args){
-        const client = message.client, APIPing = Math.round(client.ws.ping)+ "ms";
-        message.channel.send('Loading data').then (async (msg) =>{
-            msg.delete()
-            const embed = new Discord.MessageEmbed()
+	description: 'View the bots help menu',
+	execute(message, _args){
+        const embed = new Discord.MessageEmbed()
             .setColor('#eb34e8')
             .setAuthor('Help')
-            .addField("**avatar           **", `${'Displays your avatar'}` , true)
-            .addField("**invite          **", `${'redundant to be removed'}` , true)
-            .addField("**ping          **", `${'Displays the bots latency and the api latency'}` , true)
-            .addField("**serverinfo          **", `${'Displays the current servers info (has been made redundant, to be removed)'}` , true)
-            .addField("**stats           **", `${'(*new) Dispays bot info'}` , true)
-            .addField("**bruh           **", `${'Legit just bruh\'s in chat'}` , true)
-            .addField("**say          **", `${'make silica say something'}` , true)
-            .addField("**Flip          **", `${'flip a coin '}` , true)
-            .addField("**botinfo         **", `${'silicas development'}` , true)
-            .addField("**uwu         **", `${'posts gif in chat'}` , true)
-            .addField("**courbi         **", `${'mapped too only two users dont bother trying to use it'}` , true)
-            .addField("**sus       **", `${'note this command has been disabled for the foreseeable future'}` , true)
-            message.channel.send(embed).then(() => { message.channel.stopTyping() })
-        })
+            .addField("**avatar           **", 'Displays your avatar', true)
+            .addField("**invite          **", 'redundant to be removed', true)
+            .addField("**ping          **", 'Displays the bots latency and the api latency', true)
+            .addField("**serverinfo          **", 'Displays the current servers info (has been made redundant, to be removed)' , true)
+            .addField("**stats           **", '(*new) Dispays bot info' , true)
+            .addField("**bruh           **", 'Legit just bruh\'s in chat' , true)
+            .addField("**say          **", 'make silica say something' , true)
+            .addField("**Flip          **", 'flip a coin ' , true)
+            .addField("**botinfo         **", 'silicas development' , true)
+            .addField("**uwu         **", 'posts gif in chat' , true)
+            .addField("**courbi         **", 'mapped too only two users dont bother trying to use it', true)
+            .addField("**sus       **", 'note this command has been disabled for the foreseeable future', true);
+        message.channel.send(embed);
     }
 }
